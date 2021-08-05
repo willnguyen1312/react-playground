e09916f6a91e6fda8c1312e7dcaf0de53f83d23e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-05 13:20:33 +0700

    Oho date

--- src/AppTime.tsx
+++ src/AppTime.tsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import { setHours, setMinutes } from "date-fns";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function App() {
   const [startDate, setStartDate] = useState(
-    setHours(setMinutes(new Date(), 30), 16)
+    new Date("2018-01-04T01:10:54.673Z")
   );
 
+  console.log(startDate);
+  console.log(startDate.toUTCString());
+  console.log(startDate.toISOString());
+
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date as Date)}
-      showTimeSelect
-    />
+    <div
+      style={{
+        margin: 100,
+      }}
+    >
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date as Date)}
+        showTimeSelect
+        dateFormat="MMMM d, yyyy h:mm aa"
+      />
+    </div>
   );
 }

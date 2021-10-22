ffd0e64314b87efc2f8c69a9575650dab2ab3e0d
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-22 22:08:45 +0700

    ^o^

--- generateAxiosApi.js
+++ generateAxiosApi.js
@@ -1,7 +1,7 @@
 const { codegen } = require("swagger-axios-codegen");
 
 codegen({
-  methodNameMode: "shortOperationId",
+  methodNameMode: "path",
   remoteUrl:
     "https://app.swaggerhub.com/apiproxy/schema/file/apis/Aliens-apidoc/dev-aliens_api/1.0.0?format=json",
   outputDir: "./src/api",

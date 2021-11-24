d0d79076cac56783c02273d26e371b5d819ab512
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-11-24 09:03:21 +0700

    chore: calculate number of folder

--- src/AppInput.tsx
+++ src/AppInput.tsx
@@ -1,4 +1,48 @@
 import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+function calNumberOfFolders(files: (File & { path: string })[]) {
+  const result: string[] = [];
+
+  for (const file of files) {
+    const { path } = file;
+    if (!path.includes("/")) {
+      continue;
+    }
+
+    const paths = path.split("/");
+    const pathName = paths.slice(1, paths.length - 1).join("/");
+    result.push(pathName);
+  }
+
+  return new Set(result);
+}
+
+function Basic(props) {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const numberOfFolders = calNumberOfFolders(acceptedFiles as any);
+
+  const files = acceptedFiles.map((file: any) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h3>Number of folders: {numberOfFolders.size}</h3>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
 
 export default function App() {
   const [files, setFiles] = useState<File[]>([]);
@@ -13,7 +57,8 @@ export default function App() {
   };
 
   return (
-    <main>
+    <main style={{ padding: 20 }}>
+      <Basic />
       <input
         aria-label="Hello Files"
         type="file"
